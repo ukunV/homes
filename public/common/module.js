220475010a689b617ce7c7bf10be8befadb602c7
kunukkk <showklee@naver.com>
2020-09-15 20:31:53 +0900

    20/09/15_update
    updated by kunukkk

---
+++ public/common/module.js
@@ -0,0 +1,4 @@
+const setapp = document.querySelector('.navbar-setapp');
+const menu = document.querySelector('.set-menu');
+
+setapp.addEventListener('click', () => menu.classList.toggle('active'));
--- public/module.js
+++
@@ -1,4 +0,0 @@
-const setapp = document.querySelector('.navbar_setapp');
-const menu = document.querySelector('.set_menu');
-
-setapp.addEventListener('click', () => menu.classList.toggle('active'));
