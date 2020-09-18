39ee18c6543a275dda9e5cbc895bd993cef8add4
Jeongm1n <qll4359@naver.com>
2020-09-18 20:04:14 +0900

    2020-09-18
    건물주/관리인 기능,관리 페이지 빼고 완료

--- config/db_config.js
+++ config/db_config.js
@@ -1,26 +1,23 @@
-// FOR SERVER - Docker Server
+// FOR SERVER
 module.exports = {
-  connectionLimit: 30,
-  host: process.env.DATABASE_HOST,
-  user: 'root',
-  password: 'homesdb-5177',
-  database: 'homes-db',
-  dateStrings: 'date',
-  charset: 'utf8',
-  debug: false,
-  insecureAuth: true,
-};
-// 위에꺼 놔두고 밑에꺼 주석 풀어서 해 정민
+     connectionLimit: 30,
+     host: 'localhost',
+     user: 'root',
+     password: 'wjdals159!',
+     database: 'test1',
+     dateStrings: 'date',
+     charset  : 'utf8',
+     debug: false,
+    insecureAuth: true
+ };
 
-// //FOR LOCAL TEST - 정민
-// module.exports = {
-//   connectionLimit: 30,
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'wjdals159!',
-//   database: 'test1',
-//   dateStrings: 'date',
-//   charset  : 'utf8',
-//   debug: false,
-//  insecureAuth: true
-// };
+//FOR LOCAL TEST
+//module.exports = {
+  //  connectionLimit: 20,
+    //host: 'localhost',
+    //user: 'root',
+    //password: 'wjdals159!',
+    //database: 'test1',
+    //dateStrings: 'date',
+    //debug: false
+//};
