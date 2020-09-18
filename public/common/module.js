56d165ef2221b638b59bb7e0f7fb728514aa87e5
zinirun <heojj97@naver.com>
2020-09-18 23:09:11 +0900

    Add floor_delete

--- public/common/module.js
+++ public/common/module.js
@@ -7,12 +7,22 @@ document.querySelector('.set-logout').addEventListener('click', () => {
   location.href = '/process/logout';
 });
 
+const floor_list = document.getElementById('floor_list');
 // add_floor() on mgmt_register.html
 let cur_cnt = 1;
 function add_floor(e) {
   e.preventDefault();
-  let floor_list = document.getElementById('floor_list');
-  floor_list.innerHTML += `<div class='cur-floor'><div class='room-floor'>${++cur_cnt}층</div><div class='room-count'><input class='room-num' type='number' min='1' max='999' step='1' /></div></div>`;
+  floor_list.innerHTML += `<div class='cur-floor' value='${++cur_cnt}'><div class='room-floor'>${cur_cnt}층</div><div class='room-count'><input class='room-num' type='number' min='1' max='999' step='1' /></div></div>`;
 }
-floorAddButton = document.querySelector('#add_bt');
+const floorAddButton = document.querySelector('#add_bt');
 floorAddButton.addEventListener('click', add_floor);
+
+function delete_floor(e) {
+  e.preventDefault();
+  if (cur_cnt > 1) {
+    let lastTarget = floor_list.querySelector(`div[value = "${cur_cnt--}"]`);
+    floor_list.removeChild(lastTarget);
+  }
+}
+const floorDeleteButton = document.querySelector('#del_bt');
+floorDeleteButton.addEventListener('click', delete_floor);
