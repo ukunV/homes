c8dc08d6853971199f4a7f67703081ac16fb5216
zinirun <heojj97@naver.com>
2020-09-18 19:17:19 +0900

    Update Databases

--- config/db_config.js
+++ config/db_config.js
@@ -1,23 +1,26 @@
-// FOR SERVER
+// FOR SERVER - Docker Server
 module.exports = {
-     connectionLimit: 30,
-     host: 'localhost',
-     user: 'root',
-     password: 'wjdals159!',
-     database: 'test1',
-     dateStrings: 'date',
-     charset  : 'utf8',
-     debug: false,
-    insecureAuth: true
- };
+  connectionLimit: 30,
+  host: process.env.DATABASE_HOST,
+  user: 'root',
+  password: 'homesdb-5177',
+  database: 'homes-db',
+  dateStrings: 'date',
+  charset: 'utf8',
+  debug: false,
+  insecureAuth: true,
+};
+// 위에꺼 놔두고 밑에꺼 주석 풀어서 해 정민
 
-//FOR LOCAL TEST
-//module.exports = {
-  //  connectionLimit: 20,
-    //host: 'localhost',
-    //user: 'root',
-    //password: 'wjdals159!',
-    //database: 'test1',
-    //dateStrings: 'date',
-    //debug: false
-//};
+// //FOR LOCAL TEST - 정민
+// module.exports = {
+//   connectionLimit: 30,
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'wjdals159!',
+//   database: 'test1',
+//   dateStrings: 'date',
+//   charset  : 'utf8',
+//   debug: false,
+//  insecureAuth: true
+// };
