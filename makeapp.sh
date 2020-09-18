f039e87a07697c082d1a9c904d622e6b30dd239a
zinirun <heojj97@naver.com>
2020-09-18 16:44:21 +0900

    Create Server Infra

--- makeapp.sh
+++ makeapp.sh
@@ -1,4 +1,4 @@
-docker rm -f bc-app
-docker rmi -f heojj97/bluecheck:0.4
-docker build --tag heojj97/bluecheck:0.4 .
-docker run -t -d --name bc-app -p 3000:3000 --link bc-mysql:db -e DATABASE_HOST=db heojj97/bluecheck:0.4
+docker rm -f homes-server
+docker rmi -f heojj97/homes-app:0.1
+docker build --tag heojj97/homes-app:0.1 .
+docker run -t -d --name homes-server -p 3000:3000 --link homes-database:db -e DATABASE_HOST=db heojj97/homes-app:0.1
