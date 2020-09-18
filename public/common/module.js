6eb759ded822275be21f4b9d3057c452657d5ef3
kunukkk <showklee@naver.com>
2020-09-18 22:43:31 +0900

    20/09/18_update
    updated by kunukkk

--- public/common/module.js
+++ public/common/module.js
@@ -3,6 +3,18 @@ const menu = document.querySelector('.set-menu');
 
 setapp.addEventListener('click', () => menu.classList.toggle('active'));
 
-document.querySelector('.set-logout').addEventListener('click',()=>{
-	location.href = '/process/logout';
-})
+document.querySelector('.set-logout').addEventListener('click', () => {
+  location.href = '/process/logout';
+});
+
+// add_floor() on mgmt_register.html
+let cur_cnt = 1;
+function add_floor() {
+  cur_cnt++;
+  let floor_list = document.getElementById('floor_list');
+
+  floor_list.innerHTML +=
+    "<div class='cur-floor'><div class='room-floor'>" +
+    cur_cnt +
+    "층</div><div class='room-count'><input class='room-num' type='number' min='1' max='999' step='1' /></div></div>";
+}
