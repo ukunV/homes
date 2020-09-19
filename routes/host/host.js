bd7b136fc7194ceabc041be468e564f8d75342ad
Jeongm1n <qll4359@naver.com>
2020-09-20 00:20:54 +0900

    add ejs

--- routes/host/host.js
+++ routes/host/host.js
@@ -7,16 +7,23 @@ const mySqlClient = mysql.createConnection(require('../../config/db_config'));
 
 var host = function (req, res) {
 	if (req.session.user) {
-		var selectd_name;
 		const findId = req.session.user.userId;
-		const selectNameSql = 'select name from user where user_id=?';
+		const selectNameSql = 'select name from user where user_id=?;';
+		const buildingInfoSql = 'select building_name, name from buildings b, user u where b.managerID=u.user_id and b.hostID=?;';
 		mySqlClient.query(selectNameSql, findId, function(err, result){
 			selected_name = result[0].name;
-			fs.readFile('./public/host/host.html', 'utf8', function (error, data) {
-				res.send(ejs.render(data,{
-					name: selected_name
-				}));
-			});
+			mySqlClient.query(buildingInfoSql, findId, function(err, result2){
+				selected_building = result2[0].building_name;
+				selected_managerName = result2[0].name;
+				fs.readFile('./public/host/host.html', 'utf8', function (error, data) {
+					res.send(ejs.render(data,{
+						name: selected_name,
+						building_name: selected_building,
+						managerName: selected_managerName
+					}));
+				});
+			})
+			
 		});
 	} else {
 		res.send('<script type="text/javascript">alert("로그인 후 이용하세요."); window.location="/";</script>');
