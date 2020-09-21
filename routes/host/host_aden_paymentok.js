20642419da92d98d84262b954b6fbb403097dbeb
zinirun <heojj97@naver.com>
2020-09-22 02:22:05 +0900

    Update host_aden_paymentok.js

--- routes/host/host_aden_paymentok.js
+++ routes/host/host_aden_paymentok.js
@@ -1,40 +1,38 @@
 var ejs = require('ejs'),
-fs = require('fs'),
-mysql = require('mysql');
+  fs = require('fs'),
+  mysql = require('mysql');
 
 const mySqlClient = mysql.createConnection(require('../../config/db_config'));
 
 const changePaymentok = function (req, res) {
   const buildNum = req.params.id;
   const roomNum = req.params.num;
-  const paymentokSql = 'select payment_month_ok from room where buildNum=? and roomNum=?;';  
-  const updatePaymentok = 'update room set payment_month_ok=? where  buildNum=? and roomNum=?;';
+  const paymentokSql = 'select payment_month_ok from room where buildNum=? and roomNum=?;';
+  const updatePaymentokSql = 'update room set payment_month_ok=? where buildNum=? and roomNum=?;';
   if (req.session.user) {
     var alertMsg = '';
-    mySqlClient.query(paymentokSql, [buildNum, roomNum], function(err, row){
-      if(row[0]){
+    mySqlClient.query(paymentokSql, [buildNum, roomNum], function (err, row) {
+      if (row[0]) {
         const selectpaymentok = row[0].payment_month_ok;
-        if(selectpaymentok === 1){
-          mySqlClient.query(updatePaymentok, [0, buildNum, roomNum], function(err){
+        if (selectpaymentok === 1) {
+          mySqlClient.query(updatePaymentokSql, [0, buildNum, roomNum], function (err) {
             res.send(
-              '<script type="text/javascript">alert("미납으로 변경되었습니다."); window.location="/host/aden/' + buildNum + '";</script>',
-              );
+              `<script type="text/javascript">alert("미납 처리되었습니다."); window.location="/host/aden/${buildNum}";</script>`,
+            );
           });
-        }
-        else{
-          mySqlClient.query(updatePaymentok, [1, buildNum, roomNum], function(err){
+        } else {
+          mySqlClient.query(updatePaymentokSql, [1, buildNum, roomNum], function (err) {
             res.send(
-              '<script type="text/javascript">alert("완납으로 변경되었습니다."); window.location="/host/aden/' + buildNum + '";</script>',
-              );
+              `<script type="text/javascript">alert("완납 처리되었습니다."); window.location="/host/aden/${buildNum}";</script>`,
+            );
           });
         }
       }
     });
-
   } else {
     res.send(
       '<script type="text/javascript">alert("로그인 후 이용하세요."); window.location="/";</script>',
-      );
+    );
   }
 };
 
