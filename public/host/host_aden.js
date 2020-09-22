0fd13104d1376587680c98de0be2fbbb46cff672
zinirun <heojj97@naver.com>
2020-09-22 15:01:46 +0900

    update memo

---
+++ public/host/host_aden.js
@@ -0,0 +1,17 @@
+const memoOpenButtons = document.querySelectorAll('button.memo-toggle');
+
+memoOpenButtons.forEach((bt) => bt.addEventListener('click', memoToggle));
+
+function memoToggle(e) {
+  e.preventDefault();
+  const roomNum = e.target.getAttribute('room-num');
+  const target = document.querySelector(`div.room-memo-wrapper[room-num='${roomNum}']`);
+
+  if (target.style.display != 'none') {
+    target.style.display = 'none';
+    e.target.innerHTML = '🡦 메모 추가';
+  } else {
+    target.style.display = 'flex';
+    e.target.innerHTML = '🡤 메모 접기';
+  }
+}
