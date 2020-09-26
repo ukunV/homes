588c340549b755a64c8f7084d203abf086fa575e
zinirun <heojj97@naver.com>
2020-09-27 01:42:59 +0900

    Update db_config.js

--- config/db_config.js
+++ config/db_config.js
@@ -1,15 +1,15 @@
-// // FOR SERVER - Docker Server
-// module.exports = {
-//   connectionLimit: 30,
-//   host: process.env.DATABASE_HOST,
-//   user: 'root',
-//   password: 'homesdb-5177',
-//   database: 'homes-db',
-//   dateStrings: 'date',
-//   charset: 'utf8',
-//   debug: false,
-//   insecureAuth: true,
-// };
+// FOR SERVER - Docker Server
+module.exports = {
+  connectionLimit: 30,
+  host: process.env.DATABASE_HOST,
+  user: 'root',
+  password: 'homesdb-5177',
+  database: 'homes-db',
+  dateStrings: 'date',
+  charset: 'utf8',
+  debug: false,
+  insecureAuth: true,
+};
 
 // 위에 Server용 절대 지우지말것
 // 테스트할거면 밑에서 컨픽짜서 하기바람
@@ -27,15 +27,15 @@
 //  insecureAuth: true
 // };
 
-//FOR LOCAL TEST - 전진
-module.exports = {
-  connectionLimit: 30,
-  host: 'localhost',
-  user: 'root',
-  password: 'wjswls1',
-  database: 'homes-db',
-  dateStrings: 'date',
-  charset: 'utf8',
-  debug: false,
-  insecureAuth: true,
-};
+// //FOR LOCAL TEST - 전진
+// module.exports = {
+//   connectionLimit: 30,
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'wjswls1',
+//   database: 'homes-db',
+//   dateStrings: 'date',
+//   charset: 'utf8',
+//   debug: false,
+//   insecureAuth: true,
+// };
