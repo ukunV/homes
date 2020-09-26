8b21c853a9e5632a1bd716bceafdfadbe66b14c6
zinirun <heojj97@naver.com>
2020-09-26 19:58:15 +0900

    update mgr

--- public/manager/mgr_nav.js
+++ public/manager/mgr_nav.js
@@ -2,6 +2,6 @@ const navHome = document.querySelector('#nav_home');
 const navPush = document.querySelector('#nav_push');
 const navFunction = document.querySelector('#nav_function');
 
-navHome.addEventListener('click', () => (location.href = '/'));
+navHome.addEventListener('click', () => (location.href = '/manager'));
 navPush.addEventListener('click', () => (location.href = '/push'));
 navFunction.addEventListener('click', () => (location.href = '/function'));
