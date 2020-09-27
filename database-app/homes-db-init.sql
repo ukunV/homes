b0db4a70d4c82c3525f24ffc1a067746d2cb5a75
zinirun <heojj97@naver.com>
2020-09-27 19:07:25 +0900

    Update homes-db-init.sql

--- database-app/homes-db-init.sql
+++ database-app/homes-db-init.sql
@@ -62,37 +62,3 @@ content VARCHAR(5000) NOT NULL, #메시지내용
 isRead TINYINT(1) default 0, # 0:ㅗ안읽음, 1:읽음
 CONSTRAINT fk_sender FOREIGN KEY(sender) REFERENCES user(user_id),
 CONSTRAINT fk_receiver FOREIGN KEY(receiver) REFERENCES user(user_id)) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8;
-
-
-INSERT INTO `user` (`id`, `user_id`, `password`, `name`, `type`, `tel`, `token`) VALUES
-	(1, 'test1', '1ARVn2Auq2/WAqx2gNrL+q3RNjAzXpUfCXrzkA6d4Xa22yhRLy4AC50E+6UTPoscbo31nbOoq51gvkuXzJ6B2w==', '건물주다', '건물주', '01012341234', NULL),
-	(2, 'test2', '1ARVn2Auq2/WAqx2gNrL+q3RNjAzXpUfCXrzkA6d4Xa22yhRLy4AC50E+6UTPoscbo31nbOoq51gvkuXzJ6B2w==', '관리인임', '관리인', '01012341234', NULL),
-    (3, 'test3', '1ARVn2Auq2/WAqx2gNrL+q3RNjAzXpUfCXrzkA6d4Xa22yhRLy4AC50E+6UTPoscbo31nbOoq51gvkuXzJ6B2w==', '세입자1', '세입자', '01012341234', NULL),
-    (4, 'test4', '1ARVn2Auq2/WAqx2gNrL+q3RNjAzXpUfCXrzkA6d4Xa22yhRLy4AC50E+6UTPoscbo31nbOoq51gvkuXzJ6B2w==', '세입자2', '세입자', '01012341234', NULL),
-    (5, 'test5', '1ARVn2Auq2/WAqx2gNrL+q3RNjAzXpUfCXrzkA6d4Xa22yhRLy4AC50E+6UTPoscbo31nbOoq51gvkuXzJ6B2w==', '세입자3', '세입자', '01012341234', NULL),
-    (6, 'test6', '1ARVn2Auq2/WAqx2gNrL+q3RNjAzXpUfCXrzkA6d4Xa22yhRLy4AC50E+6UTPoscbo31nbOoq51gvkuXzJ6B2w==', '세입자4', '세입자', '01012341234', NULL);
-    
-INSERT INTO `buildings` (`buildingNum`, `building_name`, `hostID`, `managerID`, `building_addr`, `bank_account`) VALUES
-	(1, '에덴빌', 'test1', 'test2', '경기도 용인시 수지구 죽전로144번길 19-1', '국민 123-456789-123'),
-	(2, '파크빌', 'test1', 'test2', '경기도 용인시 수지구 죽전로144번길 19-1', '신한 123-456789-123'),
-	(3, '드림하우스', 'test1', 'test2', '경기도 용인시 수지구 죽전로144번길 19-1', '농협 123-456789-123'),
-	(4, '하이빌', 'test1', 'test1', '경기도 용인시 수지구 죽전로144번길 19-1', '기업 123-456789-123');
-
-	
-INSERT INTO `room` (`buildNum`, `roomNum`, `tenantID`) VALUES
-	(1, 101, 'test3'),
-	(1, 102, 'test4'),
-	(1, 201, 'test5'),
-	(1, 202, 'test6'),
-	(2, 101, NULL),
-	(2, 102, NULL),
-	(2, 201, NULL),
-	(2, 202, NULL),
-	(3, 101, NULL),
-	(3, 102, NULL),
-	(3, 201, NULL),
-	(3, 202, NULL),
-	(4, 101, NULL),
-	(4, 102, NULL),
-	(4, 201, NULL),
-	(4, 202, NULL);
