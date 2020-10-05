ae35389ea85425ed0de87dce9c24a7c82e0c261f
zinirun <heojj97@naver.com>
2020-10-05 19:21:32 +0900

    Add EJS Layout

--- public/host/host_nav.js
+++
@@ -1,9 +0,0 @@
-const navHome = document.querySelector('#nav_home');
-const navPush = document.querySelector('#nav_push');
-const navFunction = document.querySelector('#nav_function');
-const navManagement = document.querySelector('#nav_management');
-
-navHome.addEventListener('click', () => (location.href = '/'));
-navPush.addEventListener('click', () => (location.href = '/push'));
-navFunction.addEventListener('click', () => (location.href = '/function'));
-navManagement.addEventListener('click', () => (location.href = '/host/management'));
--- public/manager/mgr_nav.js
+++
@@ -1,7 +0,0 @@
-const navHome = document.querySelector('#nav_home');
-const navPush = document.querySelector('#nav_push');
-const navFunction = document.querySelector('#nav_function');
-
-navHome.addEventListener('click', () => (location.href = '/manager'));
-navPush.addEventListener('click', () => (location.href = '/push'));
-navFunction.addEventListener('click', () => (location.href = '/function'));
