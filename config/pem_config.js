e67099f6dc6c677ede75ae8e9ef751ccbe4bfdbe
zinirun <heojj97@naver.com>
2020-10-07 20:51:01 +0900

    Update pem

--- config/pem_config.js
+++ config/pem_config.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const keys_dir = 'config/secure/';
-const ca = fs.readFileSync(keys_dir + 'client.csr');
+const ca = fs.readFileSync(keys_dir + 'ca.ca-bundle');
 const key = fs.readFileSync(keys_dir + 'key.pem');
-const cert = fs.readFileSync(keys_dir + 'cert.pem');
+const cert = fs.readFileSync(keys_dir + 'cert.crt');
 
 module.exports.options = {
   key,
