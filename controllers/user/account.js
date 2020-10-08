0c573a6fa7bc943e876c4ad012502e63522e5212
zinirun <heojj97@naver.com>
2020-10-08 19:50:04 +0900

    Update pw-change

--- controllers/user/account.js
+++ controllers/user/account.js
@@ -14,20 +14,18 @@ const postPassword = function (req, res) {
   const Confirm = 'select id from user where user_id = ? && password = ?;';
   mySqlClient.query(Confirm, [req.session.user.userId, userPW], function (err, row) {
     if (row[0]) {
-      res.send(
-        '<script type="text/javascript">window.location="/account/management";</script>',
-        );
+      res.send('<script type="text/javascript">window.location="/account/management";</script>');
     } else {
       res.send(
         '<script type="text/javascript">alert("비밀번호가 틀렸습니다."); window.location="/check/password";</script>',
-        );
+      );
     }
   });
 };
 
 const loadAccount = function (req, res) {
   const userInfoSql = 'select user_id, type, name, tel from user where user_id=?;';
-  mySqlClient.query(userInfoSql, req.session.user.userId, function(err, row){
+  mySqlClient.query(userInfoSql, req.session.user.userId, function (err, row) {
     res.render('common/account_management.html', {
       userType: req.session.user.userType,
       ...row[0],
@@ -38,31 +36,33 @@ const loadAccount = function (req, res) {
 const postChangeTel = function (req, res) {
   const tel = req.body.tel;
   const updateTelSql = 'update user set tel = ? where user_id = ?;';
-  mySqlClient.query(updateTelSql, [tel, req.session.user.userId], function(err, row){
-    if(err){
-      res.send('<script type="text/javascript">alert("잘못된 DB 접근입니다"); window.history.back();</script>');
+  mySqlClient.query(updateTelSql, [tel, req.session.user.userId], function (err, row) {
+    if (err) {
+      res.send(
+        '<script type="text/javascript">alert("잘못된 DB 접근입니다"); window.history.back();</script>',
+      );
     } else {
-      res.send('<script type="text/javascript">alert("성공적으로 변경하였습니다."); window.location="/account/management";</script>')
+      res.send(
+        '<script type="text/javascript">alert("성공적으로 변경하였습니다."); window.location="/account/management";</script>',
+      );
     }
   });
 };
 
 const postChangePw = function (req, res) {
-  const pw = req.body.password;
-  if(pw.length > 3){
-    const password = crypto.createHash('sha512').update(req.body.password).digest('base64');
-    const updatePwSql = 'update user set password = ? where user_id = ?;';
-    mySqlClient.query(updatePwSql, [password, req.session.user.userId], function(err, row){
-      if(err){
-        res.send('<script type="text/javascript">alert("잘못된 DB 접근입니다"); window.history.back();</script>');
-      } else {
-        res.send('<script type="text/javascript">alert("비밀번호를 성공적으로 변경하였습니다.\\n다시 로그인해주세요:)"); window.location="/process/logout";</script>')
-      }
-    });
-  } else {
-    res.send('<script type="text/javascript">alert("비밀번호를 4자리 이상으로 설정하세요."); window.history.back();</script>');
-
-  }
+  const password = crypto.createHash('sha512').update(req.body.password).digest('base64');
+  const updatePwSql = 'update user set password = ? where user_id = ?;';
+  mySqlClient.query(updatePwSql, [password, req.session.user.userId], function (err, row) {
+    if (err) {
+      res.send(
+        '<script type="text/javascript">alert("잘못된 DB 접근입니다"); window.history.back();</script>',
+      );
+    } else {
+      res.send(
+        '<script type="text/javascript">alert("비밀번호를 성공적으로 변경하였습니다.\\n다시 로그인해주세요:)"); window.location="/process/logout";</script>',
+      );
+    }
+  });
 };
 
 module.exports = {
--- public/common/account_management.js
+++ public/common/account_management.js
@@ -1,4 +1,10 @@
 const modifyBts = document.querySelectorAll('.view-submit');
+const pwAgainWrapper = document.querySelector('.password-again-wrapper');
+const passwordInput = document.querySelector('#password_text');
+const passwordAgainInput = document.querySelector('#password_again');
+const pwMsg = document.querySelector('#password_msg');
+const pwForm = document.querySelector('#password_form');
+const pwSubmitBt = document.querySelector('button[mod-target="password"]');
 
 const viewSubmit = (e) => {
   e.preventDefault();
@@ -9,13 +15,44 @@ const viewSubmit = (e) => {
     viewSubmitTarget.style.display = 'block';
     e.target.classList.add('cancel');
     e.target.innerHTML = '취소';
-    inputTarget.removeAttribute('disabled');
+    inputTarget.disabled = false;
+    if (modify_target === 'password') {
+      pwAgainWrapper.style.display = 'flex';
+    }
   } else {
     viewSubmitTarget.style.display = 'none';
     e.target.classList.remove('cancel');
     e.target.innerHTML = '수정';
-    inputTarget.setAttribute('disabled', true);
+    inputTarget.disabled = true;
+    if (modify_target === 'password') {
+      pwAgainWrapper.style.display = 'none';
+    }
   }
 };
+
+function checkPassword() {
+  const pwValue = passwordInput.value;
+  const pwAgainValue = passwordAgainInput.value;
+
+  if (pwValue.length < 6) {
+    pwMsg.textContent = '6자 이상 입력';
+    pwSubmitBt.disabled = true;
+    return;
+  }
+
+  if (pwValue != pwAgainValue) {
+    pwMsg.textContent = '비밀번호 불일치';
+    pwSubmitBt.disabled = true;
+    return;
+  }
+
+  pwMsg.textContent = '';
+  pwSubmitBt.disabled = false;
+}
+
+passwordInput.addEventListener('keyup', checkPassword);
+passwordAgainInput.addEventListener('keyup', checkPassword);
+
+checkPassword();
 
 modifyBts.forEach((bt) => bt.addEventListener('click', viewSubmit));
