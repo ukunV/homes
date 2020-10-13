8857ee090fe4c30575d61c0f8a8cb3cc867ba408
zinirun <heojj97@naver.com>
2020-10-13 16:10:44 +0900

    Update tel

---
+++ public/common/phoneFormatter.js
@@ -0,0 +1,20 @@
+const tels = document.querySelectorAll('#phone_num');
+function phoneFormat(num) {
+  let formatNum = '';
+  if (num.length == 11) {
+    formatNum = num.replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3');
+  } else if (num.length == 8) {
+    formatNum = num.replace(/(\d{4})(\d{4})/, '$1-$2');
+  } else {
+    if (num.indexOf('02') == 0) {
+      formatNum = num.replace(/(\d{2})(\d{4})(\d{4})/, '$1-$2-$3');
+    } else {
+      formatNum = num.replace(/(\d{3})(\d{3})(\d{4})/, '$1-$2-$3');
+    }
+  }
+  return formatNum;
+}
+
+tels.forEach((t) => {
+  t.textContent = phoneFormat(t.textContent);
+});
