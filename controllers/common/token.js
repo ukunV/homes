5a157e052223d423e5ea85156b21b843d7cfb236
zinirun <heojj97@naver.com>
2020-10-13 16:15:42 +0900

    Update token.js

--- controllers/common/token.js
+++ controllers/common/token.js
@@ -3,7 +3,7 @@ const expo = new Expo();
 const mysql = require('mysql');
 const mySqlClient = mysql.createConnection(require('../../config/db_config'));
 
-var token = function (req, res) {
+const token = function (req, res) {
   if (req.body.token) {
     console.log(req.body.token);
     res.cookie('token', req.body.token);
@@ -14,9 +14,64 @@ var token = function (req, res) {
   }
 };
 
-var getUserTokens = function (message, type) {
-  var selectTokenSql = 'select token from user where type=?';
-  var savedTokens = [];
+// Push cases
+// 완납처리 to 세입자 (1명)
+const sendPushOfPaymentOk = (bid, rid) => {
+  const getTokenSql =
+    'SELECT token FROM user u, buildings b, room r WHERE u.user_id=r.tenantID AND b.buildingNum=r.buildNum AND buildingNum = ? AND roomNum = ?;';
+  const message = '월세가 완납처리 되었어요!';
+
+  mySqlClient.query(getTokenSql, [bid, rid], (err, row) => {
+    if (err) {
+      console.error(err);
+    } else {
+      if (row.length > 0) {
+        console.dir(row);
+        handlePushTokens(message, row[0].token);
+      }
+    }
+  });
+};
+
+// 메시지 to 수신자(공통)
+const sendPushOfMessage = (receivers, message) => {
+  const getTokenSql = 'SELECT token FROM user u WHERE u.user_id IN (?);';
+  const tokens = [];
+
+  mySqlClient.query(getTokenSql, [receivers], (err, rows) => {
+    if (err) {
+      console.error(err);
+    } else {
+      if (rows.length > 0) {
+        console.dir(rows);
+        rows.forEach((r) => tokens.push(r.token));
+        handlePushTokens(message, tokens);
+      }
+    }
+  });
+};
+
+// 유지보수 등록 to 건물주/관리인
+const sendPushOfRepair = (roomId) => {
+  const getTokenSql = 'SELECT token FROM user u WHERE u.user_id IN (?);';
+  const tokens = [];
+
+  mySqlClient.query(getTokenSql, [receivers], (err, rows) => {
+    if (err) {
+      console.error(err);
+    } else {
+      if (rows.length > 0) {
+        console.dir(rows);
+        rows.forEach((r) => tokens.push(r.token));
+        handlePushTokens(message, tokens);
+      }
+    }
+  });
+};
+
+const getUserTokensAndPush = function (message, type) {
+  const selectTokenSql = 'select token from user where type=?';
+  const savedTokens = [];
   mySqlClient.query(selectTokenSql, type, function (err, rows) {
     if (err) {
       console.log('select token err>>' + err);
@@ -63,5 +118,5 @@ const handlePushTokens = (message, savedPushTokens) => {
   })();
 };
 
-module.exports.sendPush = getUserTokens;
+module.exports.sendPush = getUserTokensAndPush;
 module.exports.addToken = token;
