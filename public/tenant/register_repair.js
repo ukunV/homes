54577f74a42cd2959e7cef929627de729e8e40c5
zinirun <heojj97@naver.com>
2020-10-18 23:29:42 +0900

    update repair, panda

--- public/tenant/register_repair.js
+++ public/tenant/register_repair.js
@@ -1,14 +1,12 @@
 const img_upload = document.querySelector('#img_upload');
-const img_thumbnails = document.querySelector('#img_thumbnail');
+const img_thumbnail = document.querySelector('#img_thumbnail');
 const img_explain = document.querySelector('#img_explain');
 
 img_upload.addEventListener('change', function (e) {
   const reader = new FileReader();
   reader.onload = (e) => {
-    let img = document.createElement('img');
-    img.setAttribute('src', e.target.result);
-    img_thumbnail.appendChild(img);
+    img_thumbnail.innerHTML = `<img src="${e.target.result}">`;
+    img_thumbnail.style.display = 'block';
   };
   reader.readAsDataURL(e.target.files[0]);
-  img_explain.style.visibility = 'hidden';
 });
