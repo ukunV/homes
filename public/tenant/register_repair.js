76b155719ebfcba8eacee996e33d6f6658d7022b
zinirun <heojj97@naver.com>
2020-10-19 22:50:17 +0900

    update reg-repair

--- public/tenant/register_repair.js
+++ public/tenant/register_repair.js
@@ -3,6 +3,7 @@ const img_thumbnail = document.querySelector('#img_thumbnail');
 const img_explain = document.querySelector('#img_explain');
 
 img_upload.addEventListener('change', function (e) {
+  img_thumbnail.innerHTML = '미리보기 로딩 중..⏳';
   const reader = new FileReader();
   reader.onload = (e) => {
     img_thumbnail.innerHTML = `<img src="${e.target.result}">`;
