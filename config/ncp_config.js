ac2820f82ee5d27486260215060c17cfc0b329ff
Jeon Jin Heo <60263249+zinirun@users.noreply.github.com>
2021-02-16 21:51:09 +0900

    Delete ncp_config.js

--- config/ncp_config.js
+++
@@ -1,6 +0,0 @@
-module.exports.sensSecret = {
-  phoneNumber: '01049414921',
-  serviceId: 'ncp:sms:kr:260975689774:homes-sms',
-  secretKey: 'pptLNIggnsYyrc7mQgDaAtQxH31rgxpiykMlX6LU',
-  accessKey: 'tdsUN2awCOZifpjNh0Ko',
-};
